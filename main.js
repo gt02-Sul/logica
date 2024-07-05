d2832ce0d36a7c5b8175ccab29eec4ccb624acc2
Carlos Vieira <112698218+carloeira@users.noreply.github.com>
2024-07-05 19:43:36 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+// Estrutura Sequencial
+
+// let primeiroNum = parseInt(prompt("digite um número:"));
+// let segundoNum = parseInt(prompt("Digite outro número:"));
+
+// let soma = primeiroNum + segundoNum;
+
+// console.log("A soma desses valores é:" + soma);
+
+
+// Estrutura de decisão
+
+// let num = parseInt(prompt("Digite um número:"));
+// if (num > 0) {
+//     console.log("O número é positivo!")
+// } else {
+//     console.log("O número é negativo!")
+// }
+
+// Estrutura de repetição
+
+// let contador = 1;
+// while (contador <= 100) {
+//     console.log("Contagem:", contador);
+//     contador++;
+// } 
+
+
+// Estrutura de Controle
+
+// for (let i = 0; i <= 10; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i, "é par");
+//     } else {
+//         console.log(i, "é impar");
+//     }
+// }
